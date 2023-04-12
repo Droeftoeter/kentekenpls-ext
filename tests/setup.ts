2fd198322c9d5ca670456e7457fd84e8edc1bbb7
Droeftoeter <freakpost@gmail.com>
2023-04-12 23:39:56 +0200

    Headful

--- tests/setup.ts
+++ tests/setup.ts
@@ -7,13 +7,15 @@ export const test = base.extend<{
 }>({
     context: async ({ }, use) => {
         const pathToExtension = path.join(__dirname, "../extension/dist/chrome");
+        const dataDir = path.join(__dirname, "../.chromium");
 
-        const context = await chromium.launchPersistentContext('', {
+        const context = await chromium.launchPersistentContext(dataDir, {
             headless: false,
             args: [
                 `--disable-extensions-except=${pathToExtension}`,
                 `--load-extension=${pathToExtension}`,
             ],
+            ignoreDefaultArgs: ['--disable-component-extensions-with-background-pages'],
         });
 
         await use(context);
