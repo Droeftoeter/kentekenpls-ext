02d0931b880dd56155d2f6baf3936d90bec2e2a2
Droeftoeter <freakpost@gmail.com>
2023-04-13 20:33:03 +0200

    Only install Chromium, GitHub reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     forbidOnly: !!process.env.CI,
     retries: process.env.CI ? 2 : 0,
     workers: 1,
-    reporter: "html",
+    reporter: process.env.CI ? "github" : "list",
     use: {
         trace: "on-first-retry"
     },
