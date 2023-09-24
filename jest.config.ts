d9a06d0f96bbeef8ec8dd58f8de867aee33b82ab
Droeftoeter <freakpost@gmail.com>
2023-09-24 10:57:02 +0200

    TS

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,9 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
+import type { Config } from "jest";
+
+const config: Config = {
   preset: "ts-jest/presets/js-with-ts",
   testEnvironment: "node",
   transformIgnorePatterns: ["node_modules/(?!ky/.*)"],
 };
+
+export default config;
