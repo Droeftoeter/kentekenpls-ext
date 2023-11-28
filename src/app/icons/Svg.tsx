a7f72ead55bd344346a02d7250beb098f3c295a3
Droeftoeter <freakpost@gmail.com>
2023-11-28 22:12:15 +0100

    Updated dependencies, removed img role from Svg

--- src/app/icons/Svg.tsx
+++ src/app/icons/Svg.tsx
@@ -6,7 +6,7 @@ export type SvgProps = JSX.IntrinsicElements["svg"] & {
 };
 
 const Svg = ({ title, desc, children, ...rest }: SvgProps) => (
-  <svg xmlns="http://www.w3.org/2000/svg" role="img" {...rest}>
+  <svg xmlns="http://www.w3.org/2000/svg" {...rest}>
     <title>{title}</title>
     {desc && <desc>{desc}</desc>}
     {children}
